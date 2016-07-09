d6a6ba64daa04f7012ba557e6c715d80f9de2d14
aluh <adriano.carapeto@yahoo.com.br>
2016-07-09 05:52:10 -0300

    Update Functions.hpp

--- Sinister.Altis/Functions.hpp
+++ Sinister.Altis/Functions.hpp
@@ -99,7 +99,7 @@ class Life_Client_Core
 		class storeVehicle {};
 		class robShops {}; //Rob Gas Station & Shops Script
 		class robAction {};
-		class fn_robShops {};
+		class robBank {};
 		class captureHideout {};
 		class gather {};
 		class gutAnimal {};
