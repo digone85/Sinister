36a926ba290ee437109d62c7145728fc55b20fa3
digone85 <rodigone@gmail.com>
2016-07-18 03:19:22 -0300

    Update cell_phone.hpp

--- MafiaTeam.Altis/dialog/cell_phone.hpp
+++ MafiaTeam.Altis/dialog/cell_phone.hpp
@@ -1,28 +1,27 @@
 class Life_cell_phone {
 	idd = 3000;
 	name= "life_cell_phone";
-	movingEnable = false;
-	enableSimulation = true;
+	movingEnable = 0;
+	enableSimulation = 1;
 	onLoad = "[] spawn life_fnc_cellphone";
 	
 	class controlsBackground {
-class fondtablet: Life_RscPicture
-		{
-			idc = 9090909;
-			text = "textures\menu.paa";
-			x = 0;
-			y = -0.12;
-			w = 1;
-			h = 1.28;
+		class Life_RscTitleBackground:Life_RscText {
+			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.7])"};
+			idc = -1;
+			x = 0.1;
+			y = 0.2;
+			w = 0.64;
+			h = (1 / 25);
 		};
 		
 		class MainBackground:Life_RscText {
 			colorBackground[] = {0, 0, 0, 0.7};
 			idc = -1;
-			x = 0;
-			y = 0;
-			w = 0;
-			h = 0;
+			x = 0.1;
+			y = 0.2 + (11 / 250);
+			w = 0.64;
+			h = 0.3 - (5 / 250);
 		};
 	};
 	
