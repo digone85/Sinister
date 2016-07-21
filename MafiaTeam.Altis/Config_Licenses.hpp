1368287e19ddbf17bfdb56fc13f040ab9de8544e
aluh <adriano.carapeto@yahoo.com.br>
2016-07-21 20:47:10 -0300

    Update Config_Licenses.hpp

--- MafiaTeam.Altis/Config_Licenses.hpp
+++ MafiaTeam.Altis/Config_Licenses.hpp
@@ -157,7 +157,7 @@ class Licenses {
     class rebel {
         variable = "rebel";
         displayName = "STR_License_Rebel";
-        price = 1450000;
+        price = 1000000;
         illegal = true;
         side = "civ";
     };
